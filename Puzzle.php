49a94766f0b6b433c4519d684a413fe6d1c7a9af
dedkazak <leben-spiel@ukr.net>
2018-04-02 19:22:54 +0300

    Вторая версия. Выделение методов

--- Puzzle.php
+++ Puzzle.php
@@ -10,36 +10,52 @@ class Puzzle
      * @var Puzzle
      */
 
-    public function __construct(int $x, int $y, int $name)
-    {
+    public function __construct(int $x, int $y, int $name) {
         $this->x = $x;
         $this->y = $y;
         $this->name = $name;
 
     }
 
+    public function viewPuzzle(string $style) {
+        if ($this->x%4 != 0) {
+            echo "<td width='30'><input type = 'submit' value='" . $this->name . "' name='char' style='".$style."'/> </td>";
+        }
+        else {
+            echo "<td width='30'><input type = 'submit' value='" . $this->name . "' name='char' style='".$style."'/> </td>";
+            echo "</tr><tr>";
+        }
+    }
+
+    public function getEmptyPuzzle() {
+        if ($this->name == 16) {
+            return $this;
+        }
+    }
+
+    public function getChoicePuzzle(int $name) {
+        if ($this->name == $name) {
+            return $this;
+        }
+    }
 
     public function getName(): int
     {
         return $this->name;
     }
 
-    public function findPuzzle (int $name, Puzzle $empty)
-    {
-        if ($this->name == $name) {
-            if ((($this->x === $empty->x) && (abs($this->y-$empty->y) == 1)) || (($this->y === $empty->y) && (abs($this->x-$empty->x) == 1))) {
-                $f = $this->name;
-                $this->name = $empty->name;
-                $empty->name = $f;
-            }
-
-
+    public function movePuzzle (Puzzle $choice) {
+        $y = abs($this->y-$choice->y);
+        $x = abs($this->x-$choice->x);
+        if ((($this->x === $choice->x) && ($y == 1)) || (($this->y === $choice->y) && ($x == 1))) {
+            $f = $this->name;
+            $this->name = $choice->name;
+            $choice->name = $f;
         }
-
     }
 
-    public function getFinish (int $i) : bool
-    {
+
+    public function getFinish (int $i) : bool {
         $result = false;
         if ($this->name == $i)
             $result = true;
--- index.php
+++ index.php
@@ -8,92 +8,63 @@ include "Puzzle.php";
 session_start();
 $names = range(1, 16);
 shuffle($names);
-//создание массива объектов+вывод на экран
-$x = 0;
-$y = 0;
-if (!isset($_POST['char'])) {
-    echo "<form method='post'><table border = 1><tr>";
-
+//заполнение массива пазлов
+function fillArray(array $names) {
+    $x = 1;
+    $y = 1;
     foreach ($names as $number) {
-        if ($number == 16) {
-            $style = "visibility:hidden;";
-        }
-        else $style = "width:50px;";
-        if ($y < 4) {
+        if ($y < 5) {
 
-            if ($x < 3) {
+            if ($x < 4) {
                 $puzzle[$number] = new Puzzle($x, $y, $number);
                 $x++;
-                echo "<td width='30'><input type = 'submit' value='" . $number . "' name='char' style='".$style."'/> </td>";
-
-            } else {
-
-
+            }
+            else {
                 $puzzle[$number] = new Puzzle($x, $y, $number);
-                echo "<td width='30'><input type = 'submit' value='" . $number . "' name='char' style='".$style."'/> </td>";
-                echo "</tr><tr>";
-                $x = 0;
+                $x = 1;
                 $y++;
             }
         }
     }
-    echo "</tr></table></form>";
-    $_SESSION['puz'] = $puzzle;
+    return $puzzle;
 }
+//вывод пазлов
+function view(array $array) {
+    echo "<form method='post'><table border = 1><tr>";
+    foreach ($array as $value) {
+        if ($value->getName() == 16) {
+            $style = "visibility:hidden;";
+        }
+        else $style = "width:50px;";
+        $value->viewPuzzle($style);
+    }
+    echo "</tr></table></form>";
+}
+function getFinish(array $array, $i = 1, $result = true) {
+    foreach ($array as $value) {
+        $result *= $value->getFinish($i);
+        $i++;
+    }
+    return $result;
+}
+
+if (!isset($_POST['char'])) {
+    $_SESSION['puz'] = fillArray($names);
+    view($_SESSION['puz']);
+}
+
 //если передвинут пазл
 else {
-    $arrayPuzzle = $_SESSION['puz'];
     $name = $_POST['char'];
-    $finish = true;
-    $keys = array_keys($arrayPuzzle);
-    //определение пустого элемента
-    foreach ($arrayPuzzle as $elem) {
-        if ($elem->getName() == 16) {
-            $empty = $elem;
-        }
-    }
-    //поиск и замена на рядом стоящий элемент с пустым
-    foreach ($arrayPuzzle as $elem) {
-        $elem->findPuzzle((int )$name, $empty);
-    }
-    //собран ли пазл?
-    foreach ($arrayPuzzle as $key => $value) {
-        $currentIndex = array_search($key, $keys) + 1;
-        $prevFinish = $value->getFinish($currentIndex);
-        $finish = $finish && $prevFinish;
+    foreach ($_SESSION['puz'] as $value) {
+        if (empty($empty))
+            $empty = $value->getEmptyPuzzle();
+        if (empty($choice))
+            $choice = $value->getChoicePuzzle((int)$name);
+        $arr[] =  $value->getName();
     }
 
-
-    if ($finish == true) {
-        echo "Игра окончена! Чтоб начать сначала, перезагрузите страницу.";
-    }
-    else {
-        echo "<form method='post'><table border = 1><tr>";
-        $x = 0;
-        $y = 0;
-        foreach ($arrayPuzzle as $number) {
-            if ($number->getName() == 16) {
-                $style = "visibility:hidden;";
-            } else $style = "width:50px;";
-            if ($y < 4) {
-
-                if ($x < 3) {
-
-                    $x++;
-                    echo "<td width='30'><input type = 'submit' value='" . $number->getName() . "' name='char' style='" . $style . "'/> </td>";
-
-                } else {
-
-                    $y++;
-                    echo "<td width='30'><input type = 'submit' value='" . $number->getName() . "' name='char' style='" . $style . "'/> </td>";
-                    echo "</tr><tr>";
-                    $x = 0;
-                }
-            }
-        }
-    }
-
-
-
-
+    $empty->movePuzzle($choice);
+    if (getFinish($_SESSION['puz'])) echo "Игра окончена! Чтоб начать сначала, перезагрузите страницу.";
+    else view($_SESSION['puz']);
 }
